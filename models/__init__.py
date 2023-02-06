2186bd651d4ee78e256c7abfa0ad79b54f3f9144
Malik <85846069+Wolverine5696@users.noreply.github.com>
2023-02-06 16:48:19 -0600

    editing key files

---
+++ models/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" this is the module for the class called BaseModel"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """this is the superclass for the airbnb project"""
+    
+    def __init__(self):
+        """initializes the object function"""
+        self.id = str(uuid.uuid4())
+        
+        
