1a76e8644fe650ad5a513f27797cd82a9296f3b0
evanrich2404 <evanrich2404@gmail.com>
2023-02-07 15:20:58 -0800

    test amenity

---
+++ tests/tests_models/test_amenity.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Amenity test module"""
+import unittest
+import models
+import os
+from datetime import datetime
+from models.amenity import Amenity
+
+
+class TestAmenityModel(unittest.TestCase):
+    """Amenity test class"""
+    def test_init(self):
+        self.assertEqual(Amenity, type(Amenity()))
+
+    def test_name_public(self):
+        hottub = Amenity()
+        self.assertEqual(str, type(Amenity.name))
+        self.assertIn("name", dir(Amenity()))
+        self.assertNotIn("name", hottub.__dict__)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
